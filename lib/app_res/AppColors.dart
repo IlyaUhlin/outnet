6440a47cfb615e76bb92ff0b835b571bafd66377
IlyaUhlin <ilyaessentemail@gmail.com>
2022-03-21 16:33:56 +0300

    Add Login screen

---
+++ lib/app_res/AppColors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color orange = Color(0xFFD78812);
+  static const Color darkPurple = Color(0xFF17073A);
+  static const Color lightPurple = Color(0xFF7B67A5);
+  static const Color lightPurple1 = Color(0xFF5C47A0);
+  static const Color grey = Color(0xFF2C2D35);
+  static const Color darkGrey = Color(0xFF1E1E23);
+}
